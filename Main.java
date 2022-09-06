49867ae09015b717dc4187d32fc236d917a5f871
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-06 16:17:57 +0000

    age calcs

--- Main.java
+++ Main.java
@@ -3,22 +3,20 @@ public class Challenge1_4
    public static void main(String[] args)
    {
        // Fill in values for these variables
-       int currentYear = 16;
+       int currentYear = 2022;
        int birthYear = 2005;
        int dogBirthYear = 2006;
 
-       // Write a formula to calculate your age
-       // from the currentYear and your birthYear variables
-       int age =
+       System.out.println(age);
+       int age = 16;
 
-       // Write a formula to calculate your dog's age
-       // from the currentYear and dogBirthYear variables
-       int dogAge =
+       System.out.println(age);
+       int dogAge = 17
 
        // Calculate the age of your dog in dogYears (7 times your dog's age in human years)
-       int dogYearsAge =
+       int dogYearsAge = dogage * 7;
 
-       // Print out your age, your dog's age, and your dog's age in dog years. Make sure you print out text too so that the user knows what is being printed out.
+      
 
 
 

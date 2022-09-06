12e3e5248261180b5e0286700738cf7206bed7fb
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-06 16:14:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Challenge1_4
+{
+   public static void main(String[] args)
+   {
+       // Fill in values for these variables
+       int currentYear = 16;
+       int birthYear = 2005;
+       int dogBirthYear = 2006;
+
+       // Write a formula to calculate your age
+       // from the currentYear and your birthYear variables
+       int age =
+
+       // Write a formula to calculate your dog's age
+       // from the currentYear and dogBirthYear variables
+       int dogAge =
+
+       // Calculate the age of your dog in dogYears (7 times your dog's age in human years)
+       int dogYearsAge =
+
+       // Print out your age, your dog's age, and your dog's age in dog years. Make sure you print out text too so that the user knows what is being printed out.
+
+
+
+   }
+}

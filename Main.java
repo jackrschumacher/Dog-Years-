302bd78e296c34fdab34fb873775e0046872e5a7
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-09-06 16:29:31 +0000

    Bug Fix

--- Main.java
+++ Main.java
@@ -11,12 +11,14 @@ public class Challenge1_4
        int age = 16;
 
        System.out.println(age);
-       int dogAge = 17
+       int dogAge = 17;
 
        // Calculate the age of your dog in dogYears (7 times your dog's age in human years)
-       int dogYearsAge = dogage * 7;
+       int dogYearsAge = (dogAge * 7);
 
-      
+      System.out.println("My age:"+age);
+      System.out.println("My dog's age:"+dogAge);
+      System.out.println("My dogs age (in human years)" + dogYearsAge);
 
 
 
